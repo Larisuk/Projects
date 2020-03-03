0e8bb4aba101d415581c9e61dfe9a88758e96dd2
Costrun Larisa-Bianca <41392462+Laris@users.noreply.github.com>
2020-03-03 05:44:45 -0800

    First commit

---
+++ alee.cpp
@@ -0,0 +1,73 @@
+﻿/*Parcul oraşului a fost neglijat mult timp, astfel că acum toate aleile sunt distruse.Prin urmare, anul acesta Primăria şi - a propus să facă reamenajări.
+Parcul are forma unui pătrat cu latura de n metri și este înconjurat de un gard care are exact două porți.Proiectanții de la Primărie au realizat o hartă a parcului și 
+au trasat pe hartă un caroiaj care împarte parcul în nxn zone pătrate cu latura de 1 metru.Astfel harta parcului are aspectul unei matrice pătratice cu n linii și n coloane.
+Liniile și respectiv coloanele sunt numerotate de la 1 la n.Elementele matricei corespund zonelor pătrate de latură 1 metru.O astfel de zonă poate să conțină un copac sau 
+este liberă.Edilii orașului doresc să paveze cu un număr minim de dale pătrate cu latura de 1 metru zonele libere(fără copaci) ale parcului, astfel încât să se obțină o alee
+continuă de la o poartă la alta.Scrieți un program care să determine numărul minim de dale necesare pentru construirea unei alei continue de la o poartă la cealaltă.*/
+#include <iostream>
+#include<fstream>
+#include<queue>
+
+using namespace std;
+
+ifstream f("alee.in");
+ofstream g("alee.out");
+
+const int dx[]={0,1,0,-1};
+const int dy[]={1,0,-1,0};
+int a[176][176];
+
+unsigned short int cx[30000];
+unsigned short int cy[30000];
+
+
+void lee(int st,int dr,int n)
+{
+   int pi,pj;
+   while(st<=dr)
+    {
+        pi=cx[st];
+        pj=cy[st];
+    
+        for(int i=0;i<4;i++)
+        {
+            int pii=pi+dx[i];
+            int pjj=pj+dy[i];
+            if(pii>0 && pjj>0 && pii<=n && pjj<=n )
+            {
+                if(a[pii][pjj]==0)
+                {
+                    a[pii][pjj]=a[pi][pj]+1;
+                    cx[dr]=pii;
+                    cy[dr]=pjj;
+                    dr++;
+                }
+            }
+        }
+       
+       st++;
+
+    }
+}
+
+
+int main()
+{
+    int n,m,x,y,xi,yi,xf,yf;
+    f>>n;
+    f>>m;
+    while(m)
+    {   f>>x>>y;
+        a[x][y]=-1;
+        m--;
+    }
+    f>>xi>>yi>>xf>>yf;
+ cx[0]=xi;
+    cy[0]=yi;
+    a[xi][yi]=1;
+    lee(0,1,n);
+    g<<a[xf][yf];
+    f.close();
+    g.close();
+    return 0;
+}
---
+++ facebook_fmi.cpp
@@ -0,0 +1,72 @@
+﻿/*Ionuț tocmai a terminat liceul și susține examenul de admitere la facultate.Știind că s - a pregătit foarte bine pentru examen, el dorește să își anunțe reușita după examen printr - o postare pe Facebook.
+Ionuț cunoaște n utilizatori reprezentați de numerele de la 1 la n, între care există m relații de prietenie de forma i j, unde i și j sunt utilizatori, iar n și m sunt numere naturale nenule.Un utilizator nu poate fi prieten cu el însuși, iar o relație de prietenie între doi utilizatori ne spune că fiecare dintre ei este prieten cu celălalt.
+
+Întrucât dorește ca postarea lui să fie cât mai răspândită, Ionuț vrea să afle care sunt utilizatorii cei mai bine conectați din mulțimea sa de cunoscuți, pentru ca eventual să le ceară prietenia.Pentru aceasta, Ionuț trebuie să găsească cea mai mare submulțime de utilizatori cunoscuți, în care fiecare utilizator din această submulțime are cel puțin k prieteni aflați la rândul lor în submulțime, unde k este un număr natural nenul.
+
+Ajutați - l pe Ionuț să se determine și să se afișeze, printr - o soluție de complexitate timp cât mai bună, în funcție de datele de intrare, membrii celei mai mari submulțimi de utilizatori, cu proprietatea că fiecare utilizator din această submulțime are cel puțin k prieteni aflați la rândul lor în submulțime.*/
+
+#include <iostream>
+        #include<fstream>
+
+        using namespace std;
+
+        ifstream f("fb_fmi.in");
+        ofstream g("fb_fmi.out");
+
+        bool a[1001][1001];
+
+       short int vecini[1001];
+
+
+
+        void verificare_vecini(int &x,int n,int k)
+        {
+            if(vecini[x]<k && vecini[x]!=0)
+           {
+               for(int i=1;i<=n;i++)
+                if(a[x][i]==1){vecini[x]--;vecini[i]--;
+                                                a[x][i]=a[i][x]=0;}
+            
+            vecini[x]=0;
+            x=0;
+           } 
+        }
+
+       
+
+
+        int main()
+        {
+            int n,x,b,i,j,m,k;
+            f>>n;
+            f>>m;
+            f>>k;
+            int cm=m;
+            while(cm)
+            {
+                f>>x;
+                f>>b;
+                a[x][b]=1;
+                a[b][x]=1;
+                vecini[x]++;
+                vecini[b]++;
+                cm--;
+
+            }
+            int numarare=0;
+
+            for(i=1;i<=n;i++)
+              {
+                verificare_vecini(i,n,k);
+
+              }
+              for(i=1;i<=n;i++)
+                if(vecini[i]>=k)numarare++;
+
+            g<<numarare<<'\n';
+            for(i=1;i<=n;i++)
+                if(vecini[i]>=k)g<<i<<" ";
+            f.close();
+            g.close();
+            return 0;
+    }
